04368cd70c2b44c23516131e357ed15b8e563fd7
Tobi Kuyoro <tobikuyo@gmail.com>
2023-03-12 21:06:35 +0000

    refactor(header): update styles and images assets

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect, useRef } from 'react';
 import { benefitsVariants, textVariants } from './Header.animations';
-import headerBackground from '@/assets/header-background.png';
-import header from '@/assets/header.png';
+import headerBackground from '@/assets/header/background.png';
+import header from '@/assets/header/main.png';
 import { useAnimationStore } from '@/hooks/store/useAnimationStore';
 import content from '@/utils/home-content.json';
 
@@ -19,41 +19,43 @@ function Header() {
 
     return (
         <div className="header">
-            <motion.div
-                className="header__text-container"
-                initial="initial"
-                animate="animate"
-                variants={textVariants}
-            >
-                <h1 className="header__heading">
-                    Take rowing to <span>the next level</span>
-                </h1>
-                <p className="header__text">{content.header}</p>
-                <Link href="/">
-                    <button className="header__button">Join Now</button>
-                </Link>
-            </motion.div>
-            <div className="header__images">
-                <Image className="header__image" src={header} alt="header" />
-                <Image
-                    className="header__image--background"
-                    src={headerBackground}
-                    alt="header background"
-                    priority
-                />
+            <div className="header__container">
                 <motion.div
-                    className="header__image-benefits"
-                    ref={benefitsRef}
+                    className="header__text-container"
                     initial="initial"
-                    whileInView="animate"
-                    variants={benefitsVariants}
-                    viewport={{ once: true }}
+                    animate="animate"
+                    variants={textVariants}
                 >
-                    <span>100+ Clubs</span>
-                    <span className="header__image-benefits--divider" />
-                    <span>Find your nearest club & enjoy the benefits</span>
-                    <span className="header__image-benefits--cta">Choose your club</span>
+                    <h1 className="header__heading">
+                        Take rowing to <span>the next level</span>
+                    </h1>
+                    <p className="header__text">{content.header}</p>
+                    <Link href="/">
+                        <button className="header__button">Join Now</button>
+                    </Link>
                 </motion.div>
+                <div className="header__images">
+                    <Image className="header__image" src={header} alt="header" />
+                    <Image
+                        className="header__image--background"
+                        src={headerBackground}
+                        alt="header background"
+                        priority
+                    />
+                    <motion.div
+                        className="header__image-benefits"
+                        ref={benefitsRef}
+                        initial="initial"
+                        whileInView="animate"
+                        variants={benefitsVariants}
+                        viewport={{ once: true }}
+                    >
+                        <span>100+ Clubs</span>
+                        <span className="header__image-benefits--divider" />
+                        <span>Find your nearest club & enjoy the benefits</span>
+                        <span className="header__image-benefits--cta">Choose your club</span>
+                    </motion.div>
+                </div>
             </div>
         </div>
     );
