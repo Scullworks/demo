e7bd77152de4a3796cd74cd39d4293d53e5ee8e1
Tobi Kuyoro <tobikuyo@gmail.com>
2023-03-22 21:30:48 +0000

    chore: change components directory structure

--- src/components/onboarding/progress-stepper/ProgressStepper.tsx
+++ src/components/onboarding/progress-stepper/ProgressStepper.tsx
@@ -10,7 +10,7 @@ const athleteSteps: Step[] = [
 ];
 
 const clubSteps: Step[] = [
-    { id: 1, label: 'Brand' },
+    { id: 1, label: 'Profile' },
     { id: 2, label: 'Details' },
     { id: 3, label: 'Services' },
     { id: 4, label: 'Boats' }
