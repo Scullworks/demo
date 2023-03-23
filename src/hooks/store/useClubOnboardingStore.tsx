fe217ec5c285cc3b0ab4f52dbc24627ac539771a
Tobi Kuyoro <tobikuyo@gmail.com>
2023-03-23 21:40:36 +0000

    refactor(useClubOnboardingStore): add services

--- src/hooks/store/useClubOnboardingStore.tsx
+++ src/hooks/store/useClubOnboardingStore.tsx
@@ -1,15 +1,25 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
+export type Service =
+    | 'Sculling'
+    | 'ERG Workout'
+    | 'Private Coaching'
+    | 'Sweep Rowing'
+    | '8x8 Sweep'
+    | 'Gear Rental';
+
 export interface OnboardingState {
     readonly imageUrl: string | null;
     readonly name: string | null;
     readonly email: string | null;
     readonly phoneNumber: number | null;
+    readonly services: Service[];
     readonly setImageUrl: (imageUrl: string) => void;
     readonly setName: (name: string) => void;
     readonly setEmail: (email: string) => void;
     readonly setPhoneNumber: (phoneNumber: number) => void;
+    readonly updateServices: (services: Service[]) => void;
 }
 
 interface ClubOnboardingState extends OnboardingState {
@@ -34,6 +44,7 @@ export const useClubOnboardingStore = create<ClubOnboardingState>()(
             closingTime: null,
             cancellationPolicy: null,
             address: null,
+            services: [],
             setImageUrl: imageUrl => set(() => ({ imageUrl })),
             setName: name => set(() => ({ name })),
             setEmail: email => set(() => ({ email })),
@@ -41,7 +52,8 @@ export const useClubOnboardingStore = create<ClubOnboardingState>()(
             setOpeningTime: openingTime => set(() => ({ openingTime })),
             setClosingTime: closingTime => set(() => ({ closingTime })),
             setCancellationPolicy: cancellationPolicy => set(() => ({ cancellationPolicy })),
-            setAddress: address => set(() => ({ address }))
+            setAddress: address => set(() => ({ address })),
+            updateServices: services => set(() => ({ services }))
         }),
         { name: 'onboarding' }
     )
