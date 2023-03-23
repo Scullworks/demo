b9b6cc93e19555306b776483bbefca32919233e7
Tobi Kuyoro <tobikuyo@gmail.com>
2023-03-23 19:45:35 +0000

    refactor: update club onboarding state store

--- src/hooks/store/useClubOnboardingStore.tsx
+++ src/hooks/store/useClubOnboardingStore.tsx
@@ -12,17 +12,36 @@ export interface OnboardingState {
     readonly setPhoneNumber: (phoneNumber: number) => void;
 }
 
-export const useClubOnboardingStore = create<OnboardingState>()(
+interface ClubOnboardingState extends OnboardingState {
+    readonly openingTime: string | null;
+    readonly closingTime: string | null;
+    readonly cancellationPolicy: string | null;
+    readonly address: string | null;
+    readonly setOpeningTime: (openingTime: string) => void;
+    readonly setClosingTime: (closingTime: string) => void;
+    readonly setCancellationPolicy: (cancellationPolicy: string) => void;
+    readonly setAddress: (address: string) => void;
+}
+
+export const useClubOnboardingStore = create<ClubOnboardingState>()(
     persist(
         set => ({
             imageUrl: null,
             name: null,
             email: null,
             phoneNumber: null,
+            openingTime: null,
+            closingTime: null,
+            cancellationPolicy: null,
+            address: null,
             setImageUrl: imageUrl => set(() => ({ imageUrl })),
             setName: name => set(() => ({ name })),
             setEmail: email => set(() => ({ email })),
-            setPhoneNumber: phoneNumber => set(() => ({ phoneNumber }))
+            setPhoneNumber: phoneNumber => set(() => ({ phoneNumber })),
+            setOpeningTime: openingTime => set(() => ({ openingTime })),
+            setClosingTime: closingTime => set(() => ({ closingTime })),
+            setCancellationPolicy: cancellationPolicy => set(() => ({ cancellationPolicy })),
+            setAddress: address => set(() => ({ address }))
         }),
         { name: 'onboarding' }
     )
