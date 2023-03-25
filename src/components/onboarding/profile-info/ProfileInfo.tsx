7b99ffa6c1827f9fc54a82fb8bd4cf3814c15e41
Tobi Kuyoro <tobikuyo@gmail.com>
2023-03-25 16:46:17 +0000

    refactor(profile-info): separate business logic

--- src/components/onboarding/profile-info/ProfileInfo.tsx
+++ src/components/onboarding/profile-info/ProfileInfo.tsx
@@ -1,81 +1,11 @@
-import { yupResolver } from '@hookform/resolvers/yup';
 import { PhotoCamera } from '@mui/icons-material';
 import { Button } from '@mui/material';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { ChangeEvent, FormEvent, useCallback, useEffect } from 'react';
-import { useForm } from 'react-hook-form';
 import { HookedTextField } from '@/components';
-import { useAuthStore, useCommonOnboardingStore, useStepperStore } from '@/hooks/store';
-import { profileSchema } from '@/utils/validations';
-
-export interface ProfileValues {
-    readonly name: string;
-}
+import { useProfileInfo } from './useProfileInfo';
 
 function ProfileInfo() {
-    const userType = useAuthStore(state => state.userType);
-    const triggerSubmit = useStepperStore(state => state.triggerSubmit);
-    const imageUrl = useCommonOnboardingStore(state => state.imageUrl);
-    const setName = useCommonOnboardingStore(state => state.setName);
-    const setImageUrl = useCommonOnboardingStore(state => state.setImageUrl);
-    const setActiveStep = useStepperStore(state => state.setActiveStep);
-    const setTriggerSubmit = useStepperStore(state => state.setTriggerSubmit);
-    const nextStep = useStepperStore(state => state.nextStep);
-
-    const router = useRouter();
-
-    const {
-        control,
-        handleSubmit,
-        formState: { errors }
-    } = useForm<ProfileValues>({
-        resolver: yupResolver(profileSchema),
-        defaultValues: {
-            name: ''
-        }
-    });
-
-    const submitDetails = useCallback(
-        () =>
-            handleSubmit(data => {
-                setName(data.name);
-                const isError = errors.name?.message !== undefined;
-
-                if (!isError) {
-                    router.push(`/onboarding/${userType}/details`);
-                    nextStep();
-                }
-            }),
-        [handleSubmit, router, setName, userType, nextStep, errors.name?.message]
-    );
-
-    function onImageInputChange(event: ChangeEvent<HTMLInputElement>) {
-        const image = event.target.files && event.target.files[0];
-        const fileReader = new FileReader();
-
-        fileReader.addEventListener('load', () => {
-            setImageUrl(fileReader.result as string);
-        });
-
-        if (image) fileReader.readAsDataURL(image);
-    }
-
-    function onSubmit(event: FormEvent) {
-        event.preventDefault();
-        submitDetails()();
-    }
-
-    useEffect(() => {
-        if (triggerSubmit) {
-            submitDetails()();
-            setTriggerSubmit(false);
-        }
-    }, [triggerSubmit, setTriggerSubmit, submitDetails]);
-
-    useEffect(() => {
-        setActiveStep(0);
-    }, [setActiveStep]);
+    const { imageUrl, onImageInputChange, onSubmit, control, errors } = useProfileInfo();
 
     return (
         <div className="profile-info">
---
+++ src/components/onboarding/profile-info/useProfileInfo.ts
@@ -0,0 +1,83 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useRouter } from 'next/router';
+import { ChangeEvent, FormEvent, useCallback, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useAuthStore, useCommonOnboardingStore, useStepperStore } from '@/hooks/store';
+import { profileSchema } from '@/utils/validations';
+
+interface ProfileValues {
+    readonly name: string;
+}
+
+export function useProfileInfo() {
+    const userType = useAuthStore(state => state.userType);
+    const triggerSubmit = useStepperStore(state => state.triggerSubmit);
+    const name = useCommonOnboardingStore(state => state.name);
+    const imageUrl = useCommonOnboardingStore(state => state.imageUrl);
+    const setName = useCommonOnboardingStore(state => state.setName);
+    const setImageUrl = useCommonOnboardingStore(state => state.setImageUrl);
+    const setActiveStep = useStepperStore(state => state.setActiveStep);
+    const setTriggerSubmit = useStepperStore(state => state.setTriggerSubmit);
+    const nextStep = useStepperStore(state => state.nextStep);
+
+    const router = useRouter();
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors, isValid }
+    } = useForm<ProfileValues>({
+        resolver: yupResolver(profileSchema),
+        defaultValues: {
+            name: name ?? ''
+        }
+    });
+
+    const submitDetails = useCallback(
+        () =>
+            handleSubmit(data => {
+                setName(data.name);
+
+                if (isValid) {
+                    router.push(`/onboarding/${userType}/details`);
+                    nextStep();
+                }
+            }),
+        [handleSubmit, isValid, nextStep, router, setName, userType]
+    );
+
+    function onImageInputChange(event: ChangeEvent<HTMLInputElement>) {
+        const image = event.target.files && event.target.files[0];
+        const fileReader = new FileReader();
+
+        fileReader.addEventListener('load', () => {
+            setImageUrl(fileReader.result as string);
+        });
+
+        if (image) fileReader.readAsDataURL(image);
+    }
+
+    function onSubmit(event: FormEvent) {
+        event.preventDefault();
+        submitDetails()();
+    }
+
+    useEffect(() => {
+        if (triggerSubmit) {
+            submitDetails()();
+            setTriggerSubmit(false);
+        }
+    }, [triggerSubmit, setTriggerSubmit, submitDetails]);
+
+    useEffect(() => {
+        setActiveStep(0);
+    }, [setActiveStep]);
+
+    return {
+        imageUrl,
+        onImageInputChange,
+        onSubmit,
+        control,
+        errors
+    };
+}
