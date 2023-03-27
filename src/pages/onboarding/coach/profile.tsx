b1fb678c61a3c2153da05a65a01391ea94d48e7a
Tobi Kuyoro <tobikuyo@gmail.com>
2023-03-27 19:25:46 +0000

    feat: create coach profile page

---
+++ src/pages/onboarding/coach/profile.tsx
@@ -0,0 +1,12 @@
+import { OnboardingLayout, ProfileInfo } from '@/components';
+
+function Profile() {
+    return (
+        <OnboardingLayout>
+            <h1>Your name and profile image</h1>
+            <ProfileInfo />
+        </OnboardingLayout>
+    );
+}
+
+export default Profile;
