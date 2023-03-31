7097cabdb56e5d22e114750533700cd5d0c82ff8
Tobi Kuyoro <tobikuyo@gmail.com>
2023-03-31 22:11:58 +0000

    feat: create function to get all clubs from firebase
    Use to populate autocomplete in athletes' and coaches' onboarding details page.

---
+++ src/services/firebase/read.ts
@@ -0,0 +1,31 @@
+import { collection, getDocs } from 'firebase/firestore';
+import { Option } from '@/components';
+import { database } from './setup';
+
+interface GetClubsResponse {
+    readonly clubs: Option[] | null;
+    readonly error: boolean;
+}
+
+export async function getClubsFromFirebase(): Promise<GetClubsResponse> {
+    try {
+        let clubs: Option[] = [];
+
+        const snapshot = await getDocs(collection(database, 'clubs'));
+        snapshot.forEach(doc => {
+            clubs = [...clubs, { id: doc.id, value: doc.data().name }];
+        });
+
+        return {
+            clubs,
+            error: false
+        };
+    } catch (error) {
+        console.error('Get Clubs Error: ', error);
+
+        return {
+            clubs: null,
+            error: true
+        };
+    }
+}
