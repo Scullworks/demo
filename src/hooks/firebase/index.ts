5a77bf7072ec90b17fb7f6246de0f29c03d9bf17
Tobi Kuyoro <tobikuyo@gmail.com>
2023-04-01 15:41:35 +0000

    feat: create hook to get athlete data

--- src/hooks/firebase/index.ts
+++ src/hooks/firebase/index.ts
@@ -1,1 +1,2 @@
+export * from './useAddAthleteData';
 export * from './useAddClubData';
---
+++ src/hooks/firebase/useAddAthleteData.ts
@@ -0,0 +1,36 @@
+import { Dayjs } from 'dayjs';
+import { serverTimestamp } from 'firebase/firestore';
+import { useAthleteOnboardingStore, useAuthStore, useCommonOnboardingStore } from '@/hooks/store';
+import { FirebaseAthlete } from '@/models';
+
+export function useAddAthleteData() {
+    const user = useAuthStore(state => state.user);
+    const name = useCommonOnboardingStore(state => state.name);
+    const phoneNumber = useCommonOnboardingStore(state => state.phoneNumber);
+    const imageUrl = useCommonOnboardingStore(state => state.imageUrl);
+
+    const dateOfBirth = useAthleteOnboardingStore(state => state.dateOfBirth) as Dayjs;
+    const emergencyName = useAthleteOnboardingStore(state => state.emergencyName);
+    const emergencyNumber = useAthleteOnboardingStore(state => state.emergencyNumber);
+
+    // Add the rest when a user selects from the available options
+    const partialAthleteData: Omit<
+        FirebaseAthlete,
+        'club' | 'membershipType' | 'positionPreference'
+    > = {
+        uid: user?.uid as string,
+        name,
+        email: user?.email as string,
+        phoneNumber,
+        dateOfBirth: dateOfBirth?.toString(),
+        emergencyName,
+        emergencyNumber,
+        createdAt: serverTimestamp(),
+        updatedAt: serverTimestamp()
+    };
+
+    return {
+        partialAthleteData,
+        imageUrl
+    };
+}
