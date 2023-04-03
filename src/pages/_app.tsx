4bcd02f95a90605cb6d66186f76f6c8517ec27eb
Tobi Kuyoro <tobikuyo@gmail.com>
2023-04-03 21:50:00 +0000

    build: install react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,27 @@ import { ThemeProvider } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { AnimatePresence } from 'framer-motion';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { theme } from '@/utils/mui/theme';
 import type { AppProps } from 'next/app';
 import '@/styles/main.scss';
 
 export default function App({ Component, pageProps, router }: AppProps) {
+    const queryClient = new QueryClient();
+
     function scrollToTop() {
         window.scrollTo(0, 0);
     }
 
     return (
-        <ThemeProvider theme={theme}>
-            <AnimatePresence mode="wait" onExitComplete={scrollToTop}>
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <Component {...pageProps} key={router.asPath} />
-                </LocalizationProvider>
-            </AnimatePresence>
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme}>
+                <AnimatePresence mode="wait" onExitComplete={scrollToTop}>
+                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                        <Component {...pageProps} key={router.asPath} />
+                    </LocalizationProvider>
+                </AnimatePresence>
+            </ThemeProvider>
+        </QueryClientProvider>
     );
 }
