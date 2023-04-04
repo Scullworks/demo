0e827a6bd8d39fc6cbd7d932ebb8d51ca974c491
Tobi Kuyoro <tobikuyo@gmail.com>
2023-04-04 22:10:40 +0000

    feat: create stripe account link

---
+++ src/pages/api/stripe/accountLinks/index.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { stripe } from '@/utils/stripe';
+
+interface CreateAccountLinkRequest extends NextApiRequest {
+    ['body']: {
+        readonly connectedAccountId: string;
+    };
+}
+
+const PAYMENTS_ENDPOINT = '/profile/club/payments';
+
+async function handler(req: CreateAccountLinkRequest, res: NextApiResponse<string>) {
+    if (req.method === 'POST') {
+        const { connectedAccountId } = req.body;
+
+        const { url } = await stripe.accountLinks.create({
+            account: connectedAccountId,
+            refresh_url: req.headers.origin + PAYMENTS_ENDPOINT,
+            return_url: req.headers.origin + PAYMENTS_ENDPOINT,
+            type: 'account_onboarding'
+        });
+
+        return res.status(201).json(url);
+    }
+}
+
+export default handler;
