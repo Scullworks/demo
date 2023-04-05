e0a613cca66eed3b57f21e5717c22a9ac859c144
Tobi Kuyoro <tobikuyo@gmail.com>
2023-04-05 20:21:24 +0000

    refactor(avatar): display initials for users with no image

--- src/components/profile/avatar/Avatar.tsx
+++ src/components/profile/avatar/Avatar.tsx
@@ -8,12 +8,19 @@ interface AvatarProps {
 function Avatar(props: AvatarProps) {
     const { profileImage, name } = props;
 
+    const initials = name?.split(' ');
+    const displayName = initials
+        ? initials[0]?.charAt(0) + initials[1]?.charAt(0)
+        : name?.charAt(0);
+
     return (
         <>
             {profileImage ? (
                 <MuiAvatar src={profileImage} alt={name ?? ''} sx={{ width: 100, height: 100 }} />
             ) : (
-                <MuiAvatar sx={{ width: 100, height: 100 }}>T</MuiAvatar>
+                <MuiAvatar sx={{ width: 100, height: 100, backgroundColor: 'rgb(0, 128, 128)' }}>
+                    {displayName}
+                </MuiAvatar>
             )}
         </>
     );
