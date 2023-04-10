dae1a80bbb23af45f253f99f37bac7f81f7a4d59
Tobi Kuyoro <tobikuyo@gmail.com>
2023-04-10 14:15:03 +0000

    feat: create session card component

---
+++ src/components/profile/session-container/SessionCard.tsx
@@ -0,0 +1,76 @@
+import { AvatarGroup, Avatar as MuiAvatar } from '@mui/material';
+import dayjs from 'dayjs';
+import { QueryObserverResult, RefetchOptions, RefetchQueryFilters } from 'react-query';
+import { Avatar } from '@/components';
+import { FirebaseClub, FirebaseSession, UserType } from '@/models';
+import { useSessionCard } from './useSessionCard';
+
+interface SessionCardProps {
+    readonly session: FirebaseSession;
+    readonly club: FirebaseClub | undefined;
+    readonly as: UserType;
+    readonly refetch: <TPageData>(
+        options?: RefetchOptions & RefetchQueryFilters<TPageData>
+    ) => Promise<QueryObserverResult>;
+}
+
+function SessionCard(props: SessionCardProps) {
+    const { session } = props;
+    const { buttonText, onClick } = useSessionCard({ ...props });
+
+    return (
+        <div className="profile-session-card" key={session.id}>
+            {session.coach ? (
+                <>
+                    <Avatar
+                        name={session.coach.name ?? null}
+                        profileImage={session.coach.profileImageRef}
+                    />
+                    <p className="profile-session-card__price">${formatPrice(session.price)}</p>
+                    <h3 className="profile-session-card__heading">{session.type}</h3>
+                    <p className="profile-session-card__coach">Coach {session.coach.name}</p>
+                </>
+            ) : (
+                <>
+                    <h3 className="profile-session-card__heading">{session.type}</h3>
+                    <p className="profile-session-card__price">${formatPrice(session.price)}</p>
+                </>
+            )}
+            <p className="profile-session-card__date">
+                {dayjs(session.date.toDate()).format('MMMM Do YYYY')}
+            </p>
+            <p className="profile-session-card__time">{session.time}</p>
+            {session.attendees && (
+                <>
+                    <p className="profile-session-card__attendees">Athletes Attending:</p>
+                    {/* TODO: Replace with actual functionality, after athlete dashboard implementation */}
+                    <AvatarGroup max={4} total={6}>
+                        <MuiAvatar alt="John Doe">JD</MuiAvatar>
+                        <MuiAvatar alt="John Doe">JD</MuiAvatar>
+                        <MuiAvatar alt="John Doe">JD</MuiAvatar>
+                        <MuiAvatar alt="John Doe">JD</MuiAvatar>
+                        <MuiAvatar alt="John Doe">JD</MuiAvatar>
+                        <MuiAvatar alt="John Doe">JD</MuiAvatar>
+                        <MuiAvatar alt="John Doe">JD</MuiAvatar>
+                    </AvatarGroup>
+                </>
+            )}
+            <button className="button__static" onClick={() => onClick(session)}>
+                {buttonText}
+            </button>
+        </div>
+    );
+}
+
+export default SessionCard;
+
+function formatPrice(price: number) {
+    const priceSplit = String(price).split('.');
+    const cents = priceSplit[1];
+
+    if (cents?.length === 1) {
+        return price + '0';
+    } else {
+        return price.toString();
+    }
+}
---
+++ src/components/profile/session-container/useSessionCard.ts
@@ -0,0 +1,53 @@
+import { deleteDoc, doc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { QueryObserverResult, RefetchOptions, RefetchQueryFilters } from 'react-query';
+import { useAuthStore } from '@/hooks/store';
+import { FirebaseClub, FirebaseSession, UserType } from '@/models';
+import { database } from '@/services/firebase';
+
+interface UseSessionCardProps {
+    readonly session: FirebaseSession;
+    readonly club: FirebaseClub | undefined;
+    readonly as: UserType;
+    readonly refetch: <TPageData>(
+        options?: RefetchOptions & RefetchQueryFilters<TPageData>
+    ) => Promise<QueryObserverResult>;
+}
+
+export function useSessionCard(props: UseSessionCardProps) {
+    const { session, club, as: userType, refetch } = props;
+    const [buttonText, setButtonText] = useState('');
+    const currentUser = useAuthStore(state => state.user);
+
+    function onClick(session: FirebaseSession) {
+        if (userType === 'club') deleteSession(session.id);
+        if (userType === 'coach' && session.coach?.id === currentUser?.uid) {
+            deleteSession(session.id);
+        }
+    }
+
+    async function deleteSession(sessionId: string) {
+        if (!club) return;
+
+        try {
+            const sessionRef = doc(database, 'clubs', club.id, 'sessions', sessionId);
+            await deleteDoc(sessionRef);
+            refetch();
+        } catch (error) {
+            console.error('Delete Session Error: ', error.message);
+        }
+    }
+
+    useEffect(() => {
+        if (userType === 'athlete') setButtonText('Attend Session');
+        if (userType === 'club') setButtonText('Cancel Session');
+        if (userType === 'coach' && session.coach?.id === currentUser?.uid) {
+            setButtonText('Cancel Session');
+        }
+    }, [userType, setButtonText, session.coach?.id, currentUser?.uid]);
+
+    return {
+        buttonText,
+        onClick
+    };
+}
