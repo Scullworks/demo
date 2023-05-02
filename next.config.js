d07cb25b1908d85df3225d6995b363457fb38f3e
Tobi Kuyoro <tobikuyo@gmail.com>
2023-05-02 21:04:25 +0100

    feat: add pwa functionality

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+});
+
+const nextConfig = withPWA({
     reactStrictMode: true
-};
+});
 
 module.exports = nextConfig;
