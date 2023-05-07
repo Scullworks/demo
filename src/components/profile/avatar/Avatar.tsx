3a0569209728ed2f7d55368e71e77a8610b1fd6d
Tobi Kuyoro <tobikuyo@gmail.com>
2023-05-08 00:40:40 +0100

    fix: bug showing undefined name

--- src/components/profile/avatar/Avatar.tsx
+++ src/components/profile/avatar/Avatar.tsx
@@ -10,9 +10,10 @@ function Avatar(props: AvatarProps) {
     const { profileImage, name, isProfileMenu } = props;
 
     const initials = name?.split(' ');
-    const displayName = initials
-        ? initials[0]?.charAt(0) + initials[1]?.charAt(0)
-        : name?.charAt(0);
+    const displayName =
+        initials && initials.length > 1
+            ? initials[0].charAt(0) + initials[initials.length - 1].charAt(0)
+            : name?.charAt(0);
 
     if (profileImage) {
         return <MuiAvatar src={profileImage} alt={name ?? ''} sx={{ width: 100, height: 100 }} />;
